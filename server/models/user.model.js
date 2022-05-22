d91b7f362a92679286779383ae5f1ce1ca328d47
Harshit Mishra <harshitlove28@outlook.com>
2022-05-22 17:26:52 +0530

    Category  added

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,6 +3,7 @@ const User=new mongoose.Schema({
     name : {type: String,required :true},
     email : {type: String, required:true, unique:true},
     password : {type: String, required : true},
+    category : {type: String, required : true},
     quote : {type : String}
 },
 {collection : 'user-data'}
